f22372a6a17f6a6349341ae91ff6f03db974c702
Invigo <invigo.devel@gmail.com>
2020-05-01 23:05:00 +0300

    Update RomanNumerals.hs

--- 7kyu/roman-numerals/src/RomanNumerals.hs
+++ 7kyu/roman-numerals/src/RomanNumerals.hs
@@ -5,18 +5,18 @@ solution n = roman n
 
 roman :: Integer -> String
 roman n
-    | n >= 1000 = "M" ++ roman (n - 1000)
-    | n >= 900  = "CM" ++ roman (n - 900)
-    | n >= 500  = "D" ++ roman (n - 500)
-    | n >= 400  = "CD" ++ roman (n - 400)
-    | n >= 100  = "C" ++ roman (n - 100)
-    | n >= 90   = "XC" ++ roman (n - 90)
-    | n >= 50   = "L" ++ roman (n - 50)
-    | n >= 40   = "XL" ++ roman (n - 40)
-    | n >= 10   = "X" ++ roman (n - 10)
-    | n >= 9    = "IX" ++ roman (n - 9)
-    | n >= 5    = "V" ++ roman (n - 5)
-    | n >= 4    = "IV" ++ roman (n - 4)
-    | n >= 1    = "I" ++ roman (n - 1)
+    | n >= 1000 = 'M' : roman (n - 1000)
+    | n >= 900  = 'C' : 'M' : roman (n - 900)
+    | n >= 500  = 'D' : roman (n - 500)
+    | n >= 400  = 'C' : 'D' : roman (n - 400)
+    | n >= 100  = 'C' : roman (n - 100)
+    | n >= 90   = 'X' : 'C' : roman (n - 90)
+    | n >= 50   = 'L' : roman (n - 50)
+    | n >= 40   = 'X' : 'L' : roman (n - 40)
+    | n >= 10   = 'X' : roman (n - 10)
+    | n >= 9    = 'I' : 'X' : roman (n - 9)
+    | n >= 5    = 'V' : roman (n - 5)
+    | n >= 4    = 'I' : 'V' : roman (n - 4)
+    | n >= 1    = 'I' : roman (n - 1)
     | n == 0    = ""
-    | otherwise = error "not well formed arabic " ++ show n
+    | otherwise = error "not well formed arabic " : show n
